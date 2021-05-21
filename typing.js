347f84f87fc0095357b723359041c94f09a4f9ba
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2021-05-21 21:12:31 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,1 +1,57 @@
+function TypingEffect(query, wordList, wait) {
+   
+    wait = parseInt(wait)
+    let text = "";
+    let index = 0;
+    let delVal = false;
+    let speed = 500;
+    
+    type();
+    function type() {
+        
+        let curIndex = index%wordList.length;
+        let word = wordList[curIndex];
+        console.log(curIndex , word);
+        if(delVal) {
+
+            text = word.substring(0, text.length-1);
+        }
+        else {
+
+            text = word.substring(0, text.length+1);
+        }
+        //console.log(word);
+        query.innerHTML = text;
+        speed = 500
+        if(delVal) {
+
+            speed /= 2;
+        }
+
+        if(!delVal && text === word) {
+
+            delVal = true;
+            speed = wait;
+        }
+        else if(delVal && text === "") {
+
+            delVal = false;
+            index++;
+        }
+
+        setTimeout(()=>type(), speed);
+    }
+   
+}
+
+init()
+function init() {
+
+    var txt = document.querySelector('.text-effect');
+    const wordList = JSON.parse(txt.getAttribute('data-words'));
+    const wait = txt.getAttribute('data-wait');
+    console.log(wait);
+    console.log(txt, wordList)
+    TypingEffect(txt, wordList, wait)
+}
 
